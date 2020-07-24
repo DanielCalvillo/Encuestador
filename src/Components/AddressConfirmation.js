f7b1c31bce285cce02207102af0c257a0638c971
Daniel Calvillo <danielcalvillomancilla123@gmail.com>
2020-07-24 18:17:06 -0500

    Handle Direction Form state

--- src/Components/AddressConfirmation.js
+++ src/Components/AddressConfirmation.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/styles';
+import PropTypes from 'prop-types';
 
-const useStyles = makeStyles((theme) => ({
+const styles = () => ({
   formHouseNumbers: {
     display: 'flex',
     justifyContent: 'center'
@@ -12,106 +14,152 @@ const useStyles = makeStyles((theme) => ({
   formExtNumber: {
     marginRight: "20px"
   }
-}));
+});
 
-function AddressConfirmation() {
+class AddressConfirmation extends Component {
+  constructor(props){
+    super(props)
+    this.state= {
+      calle: "",
+      numExterior: "",
+      numInterior: "",
+      colonia: "",
+      codPostal: "",
+      estado: ""
+    }
+  }
 
-  const handleClick = e => {
+  handleChange = e => {
+    this.setState({
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+    });
+  };
+
+  handleClick = e => {
     console.log('Button was clicked');
   };
 
-  const handleSubmit = e => {
+  handleSubmit = e => {
     e.preventDefault();
     console.log('Form was submitted');
     console.log(this.state);
   };
 
-    const classes = useStyles();
-    return (
-      <div>
-        <h1>Dirección</h1>
+    render(){
+      const { classes } = this.props;
+      const { calle, numExterior, numInteror, colonia, codPostal, estado } = this.state
 
-        <form onSubmit={handleSubmit}>
-
-          <TextField
-            size="small"
-            label="calle"
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
-          />
-
-          <div className={classes.formHouseNumbers}>
+      return (
+        <div>
+          <h1>Dirección</h1>
+  
+          <form onSubmit={this.handleSubmit}>
+  
             <TextField
-            classes={{
-              root: classes.formExtNumber
-            }}
-            label="#Exterior"
-            size="small"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
-            margin='normal'
+              onChange={this.handleChange}
+              size="small"
+              label="calle"
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              name="calle"
+              value={calle}
             />
+  
+            <div className={classes.formHouseNumbers}>
+              <TextField
+                onChange={this.handleChange}
+                classes={{
+                  root: classes.formExtNumber
+                }}
+                label="#Exterior"
+                size="small"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="outlined"
+                margin='normal'
+                name="numExterior"
+                value={numExterior}
+              />
+
+              <TextField
+                onChange={this.handleChange}
+                label="#Interior"
+                size="small"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="outlined"
+                margin='normal'
+                name="numInterior"
+                value={numInteror}
+              />
+            </div>
+  
             <TextField
-            label="#Interior"
-            size="small"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
-            margin='normal'
+              onChange={this.handleChange}
+              size="small"
+              label="colonia"
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              name="colonia"
+              value={colonia}
             />
-          </div>
-
-          <TextField
-            size="small"
-            label="Colonia"
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
-          />
-
-          <TextField
-            size="small"
-            label="Código Postal"
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
-          />
-
-          <TextField
-            size="small"
-            label="Estado"
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
-          />
-
-          <Button 
-            variant="contained" 
-            color="primary" 
-            onClick={handleClick}
-            size="large"
-          >
-          Confirmar Dirección
-          </Button>
-        </form>
-      </div>
-    );
+  
+            <TextField
+              onChange={this.handleChange}
+              size="small"
+              label="Código Postal"
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              name="codPostal"
+              value={codPostal}
+            />
+  
+            <TextField
+              onChange={this.handleChange}
+              size="small"
+              label="Estado"
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              name="estado"
+              value={estado}
+            />
+  
+            <Button 
+              type="submit"
+              variant="contained" 
+              color="primary" 
+              onClick={this.handleClick}
+              size="large"
+            >
+            Confirmar Dirección
+            </Button>
+          </form>
+        </div>
+      );
+    }
 }
 
-export default AddressConfirmation;
+AddressConfirmation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AddressConfirmation);
